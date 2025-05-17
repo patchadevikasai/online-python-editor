4266183429501f38f6ddbf592d73b4ff7f8d4070
patchadevikasai <devikasai50@gmail.com>
2025-05-17 10:06:48 +0530

    Add requirements.txt to backend

--- backend/app.py
+++ backend/app.py
@@ -41,4 +41,5 @@ def run_code():
             os.remove(filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT env var or fallback to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
